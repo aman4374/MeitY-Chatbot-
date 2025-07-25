1864e4b951fc53c17857ef79f14071909db3a430
Aman Trivedi <amantrivedi4374@gmail.com>
2025-07-25 10:04:39 +0530

    app.py

--- app.py
+++ app.py
@@ -88,13 +88,13 @@
 #         st.markdown(f"{a}")
 #         st.markdown("---")
 
-# import streamlit as st
-# import sys
-# import platform
+import streamlit as st
+import sys
+import platform
 
-# st.set_page_config(page_title="Azure Test App", layout="wide")
-# st.title("✅ Success! Your Azure App Service is Working.")
-# st.write("This is a minimal Streamlit application.")
-# st.info(f"Python Version: {sys.version}")
-# st.info(f"Platform: {platform.platform()}")
-# st.balloons()
+st.set_page_config(page_title="Azure Test App", layout="wide")
+st.title("✅ Success! Your Azure App Service is Working.")
+st.write("This is a minimal Streamlit application.")
+st.info(f"Python Version: {sys.version}")
+st.info(f"Platform: {platform.platform()}")
+st.balloons()
